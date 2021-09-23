043c5b9a22d80ae0aa8d6848db1c758b7c85afe0
zhangchao29 <zhangchao29@xiaomi.com>
2021-09-23 12:56:48 +0800

    rebuild BST

--- src/java/com/will/RebuildBST.java
+++ src/java/com/will/RebuildBST.java
@@ -1,5 +1,6 @@
 package com.will;
 
+//  重建二叉树
 public class RebuildBST {
     public TreeNode reConstructBinaryTree(int [] pre,int [] vin) {
         if(pre.length<1||vin.length<1){
@@ -15,8 +16,8 @@ public class RebuildBST {
         TreeNode root = new TreeNode(pre[preStart]);
         for(int i=vinStart;i<=vinEnd;i++){
             if(vin[i]==root.val){
-                TreeNode left = reConstructBinaryTreeInner(pre,preStart+1,preStart+1+i-vinStart,vin,vinStart,i-1);
-                TreeNode right = reConstructBinaryTreeInner(pre,i+1,preEnd,vin,i+1,vinEnd);
+                TreeNode left = reConstructBinaryTreeInner(pre,preStart+1,preStart+i-vinStart,vin,vinStart,i-1);
+                TreeNode right = reConstructBinaryTreeInner(pre,preStart+i-vinStart+1,preEnd,vin,i+1,vinEnd);
                 root.left = left;
                 root.right = right;
                 return root;
@@ -36,6 +37,14 @@ public class RebuildBST {
     }
 
     public static void main(String[] args) {
-
+//        int[] pre=new int[]{1,2,4,7,3,5,6,8};
+//        int[] vin=new int[]{4,7,2,1,5,3,8,6};
+//        int[] pre=new int[]{1};
+//        int[] vin=new int[]{1};
+        int[] pre=new int[]{4,2,7,6,8,9,3,2};
+        int[] vin=new int[]{7,2,6,4,3,2,9,8};
+        RebuildBST rebuildBST = new RebuildBST();
+        TreeNode root= rebuildBST.reConstructBinaryTree(pre,vin);
+        System.out.println();
     }
 }
