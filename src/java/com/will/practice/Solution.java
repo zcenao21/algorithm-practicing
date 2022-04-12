83f1c2e7477c9b9d7bd10775f9d396603037cdd9
zhangchao29 <zhangchao29@xiaomi.com>
2022-04-12 08:58:12 +0800

    more

--- src/java/com/will/practice/Solution.java
+++ src/java/com/will/practice/Solution.java
@@ -1,32 +1,12 @@
 package com.will.practice;
-
-
-import org.w3c.dom.Element;
-
-import javax.swing.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Random;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 
 class TreeNode {
@@ -52,134 +32,68 @@ class TreeNode {
     }
 }
 
-public class Solution {
+
+public class Solution implements Serializable {
     public static void main(String[] args) {
         Solution s = new Solution();
-        List<String> wordList = new LinkedList<>();
-//        String[] strArr = new String[]{"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
-//        wordList=Arrays.asList(strArr);
-//        List<List<String>> result = s.findLadders("qa","sq",wordList);
-
-//        String[] strArr = new String[]{"hot","dot","dog","lot","log","cog"};
-//        wordList=Arrays.asList(strArr);
-//        List<List<String>> result = s.findLadders("hit","cog",wordList);
-
-//        String[] strArr = {"kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"};
-//        wordList=Arrays.asList(strArr);
-//        List<List<String>> result = s.findLadders("cet","ism",wordList);
-
-        String[] strArr = new String[]{"hot", "dog"};
-        wordList = Arrays.asList(strArr);
-//        List<String> result = s.restoreIpAddresses("25525511135");
-//        int[][] grid =
-//                {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-//                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
-        int[][] grid =
-                {{1, 0, 1},
-                        {1, 0, 0},
-                        {0, 1, 1}};
-//        List<List<Integer>> result = s.combinationSum(new int[]{2,3,6,7}, 7);
-//        List<List<Integer>> result = s.combinationSum(new int[]{2,3,5}, 8);
-//        List<List<Integer>> result = s.combinationSum(new int[]{2}, 1);
-//        List<List<Integer>> result = s.combinationSum(new int[]{1}, 1);
-//        boolean result = s.canPartitionKSubsets(new int[]{5,2,3,3,3,4}, 4);
-//        boolean result = s.canPartitionKSubsets(new int[]{605,454,322,218,8,19,651,2220,175,710,2666,350,252,2264,327,1843},4);
-//        int result = s.longestCommonSubsequence("horse","ros");
-//        int result = s.longestCommonSubsequence("bsbininm","jmjkbkjkv");
-//        int result = s.longestCommonSubsequence("aaaaaaa","aaaa");
-//        int result = s.longestCommonSubsequence("abcd","abcdefg");
-//        int result = s.longestCommonSubsequence("acdeb","abcdefg");
-        int result = s.longestIncreasingPath(new int[][]{{3,4,5},{3,2,6},{2,2,1}});
-//        boolean result = s.canCross(new int[]{0,1,2,3,4,8,9,11});
-//        boolean result = s.canCross(new int[]{0,1});
+//        List<Integer> result=s.findMinHeightTrees(4,new int[][]{{1,0},{1,2},{1,3}});
+//        List<Integer> result=s.findMinHeightTrees(6,new int[][]{{3,0},{3,1},{3,2},{3,4},{5,4}});
+//        List<Integer> result=s.findMinHeightTrees(4,new int[][]{{3,0},{3,1},{1,9},{3,2},{2,6},{6,8},{6,7},{3,4},{4,5},{4,10},{10,11}});
+//        List<Integer> result=s.findMinHeightTrees(1,new int[][]{});
+        List<Integer> result=s.findMinHeightTrees(2,new int[][]{{0,1}});
         System.out.println(result);
-//        List<String> result = s.restoreIpAddresses("1111");
-//        List<String> result = s.restoreIpAddresses("0279245587303");
-
-
-//        TreeNode node1 = new TreeNode(6);
-//        TreeNode node2 = new TreeNode(2);
-//        TreeNode node3 = new TreeNode(8);
-//        TreeNode node4 = new TreeNode(0);
-//        TreeNode node5 = new TreeNode(4);
-//        TreeNode node6 = new TreeNode(7);
-//        TreeNode node7 = new TreeNode(9);
-//        TreeNode node8 = new TreeNode(3);
-//        TreeNode node9 = new TreeNode(5);
-//        node1.left = node2;
-//        node1.right = node3;
-//        node2.left=node4;
-//        node2.right=node5;
-//        node5.left=node8;
-//        node5.right=node9;
-//        node3.left=node6;
-//        node3.right=node7;
-//        TreeNode result = s.inorderSuccessor(node1,node1);
-//        System.out.println(result);
-//
-//        TreeNode nodeCopy1 = new TreeNode(4);
-//        TreeNode nodeCopy2 = new TreeNode(1);
-//        TreeNode nodeCopy3 = new TreeNode(2);
-//        nodeCopy1.left = nodeCopy2;
-//        nodeCopy1.right = nodeCopy3;
-//
-//        System.out.println(s.isSubtree(node1,nodeCopy1));
-
-//        int[][] grid =
-//        {{0,0,0},
-//        {1,1,0},
-//        {0,0,0},
-//        {0,1,1},
-//        {0,0,0}};
-
     }
 
-    class Element{
-        int i;
-        int j;
-        int ele;
-        public Element(int i,int j,int ele){
-            this.i=i;
-            this.j=j;
-            this.ele=ele;
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if(n<=1){
+            List<Integer> result = new LinkedList<>();
+            result.add(0);
+            return  result;
         }
-    }
+        Map<Integer, Set<Integer>> nodeAndNext = new HashMap<>();
+        for(int[] edge:edges){
+            int first = edge[0];
+            int second = edge[1];
+            Set<Integer> firstSet = nodeAndNext.containsKey(first)?nodeAndNext.get(first):new HashSet<>();
+            firstSet.add(second);
+            nodeAndNext.put(first,firstSet);
 
-    public int longestIncreasingPath(int[][] matrix) {
-        if(matrix==null||matrix.length<1) return 0;
-        int[][] maxInc = new int[matrix.length][matrix[0].length];
-        Queue<Element> elements=new PriorityQueue<>((e1, e2) -> {
-            if(e1.ele<e2.ele){
-                return -1;
-            }else{
-                return 1;
-            }
-        });
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length;j++){
-                elements.add(new Element(i,j,matrix[i][j]));
+            Set<Integer> secondSet = nodeAndNext.containsKey(second)?nodeAndNext.get(second):new HashSet<>();
+            secondSet.add(first);
+            nodeAndNext.put(second,secondSet);
+        }
+        Queue<Integer> q = new LinkedList<>();
+
+        List<Integer> result = new LinkedList<>();
+        // 得到只有叶节点
+        for(Map.Entry<Integer,Set<Integer>> node:nodeAndNext.entrySet()){
+            if(node.getValue().size()==1){
+                q.add(node.getKey());
+                result.add(node.getKey());
             }
         }
-        while (elements.size()>0){
-            System.out.println(elements.poll().ele);
+        int size = q.size();
+        while(q.size()>0){
+            Integer curr = q.poll();
+            result.add(curr);
+
+            Set<Integer> currValue = nodeAndNext.get(curr);
+            for(Integer v:currValue){
+                Set<Integer> vNext = nodeAndNext.get(v);
+                vNext.remove(curr);
+                if(vNext.size()==1){
+                    q.add(v);
+                }
+            }
+
+            if(--size==0){
+                size = q.size();
+                if(size==0){
+                    return result;
+                }
+                result = new LinkedList<>();
+            }
         }
-        return 1;
+        return result;
     }
 }
