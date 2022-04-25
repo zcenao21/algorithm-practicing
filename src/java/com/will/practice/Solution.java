b58eeabd7721c36444dfcb4722ef999ba3ea6c25
zhangchao29 <zhangchao29@xiaomi.com>
2022-04-25 21:00:30 +0800

    more

--- src/java/com/will/practice/Solution.java
+++ src/java/com/will/practice/Solution.java
@@ -1,129 +1,49 @@
 package com.will.practice;
+
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-    public String toString(){
-        return this.val+"";
-    }
-}
-
-
-//给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
-//
-//算法的时间复杂度应该为 O(log (m+n)) 。
-//
-//
-//
-//示例 1：
-//
-//输入：nums1 = [1,3], nums2 = [2]
-//输出：2.00000
-//解释：合并数组 = [1,2,3] ，中位数 2
-//示例 2：
-//
-//输入：nums1 = [1,2], nums2 = [3,4]
-//输出：2.50000
-//解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
-//
-//来源：力扣（LeetCode）
-//链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
-//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 public class Solution implements Serializable {
     public static void main(String[] args) {
         Solution s = new Solution();
-        
-        LRUCache cache = new LRUCache(2);
-        
+        Map<String,String> map = new HashMap<>();
+        map.put("A","A1");
+        map.put("A1","A2");
+        map.put("A2","A3");
+        map.put("A3","A4");
+        map.put("B","B1");
+        map.put("B1","B2");
+        map.put("B2","B3");
+
+        Map<String,String> result = s.findFirst(map);
+        System.out.println(result.get("A2"));
+        System.out.println(result.get("A1"));
+        System.out.println(result.get("A3"));
+        System.out.println(result.get("B1"));
+        System.out.println(result.get("B2"));
+        System.out.println(result.get("B3"));
+    }
+
+    public Map<String,String> findFirst(Map<String,String> map){
+        Map<String,String> firstMap = new HashMap<>();
+        for(Map.Entry<String,String> entry:map.entrySet()) {
+            firstMap.put(entry.getValue(),entry.getKey());
+        }
+        for(Map.Entry<String,String> entry:map.entrySet()){
+            String out = entry.getValue();
+            String next = out;
+            if(firstMap.containsKey(out)){
+                next = firstMap.get(out);
+                while(firstMap.containsKey(next)){
+                    next = firstMap.get(next);
+                }
+            }
+            firstMap.put(out,next);
+        }
+        return firstMap;
     }
 
 }
 
-class LRUCache {
-    class InnerEle{
-        Integer key;
-        Integer value;
-        InnerEle upper;
-        InnerEle next;
-        private InnerEle(Integer key, Integer value){
-            this.key=key;
-            this.value= value;
-        }
-    }
-    
-    int capacity=0,currSize=0;
-    InnerEle start=new InnerEle(0,0);
-    InnerEle last=start;
-    HashMap<Integer,InnerEle> map = new HashMap<>();
-    
-    public LRUCache(int capacity) {
-        this.capacity=capacity;
-    }
-    
-    public int get(int key) {
-        if(map.containsKey(key)){
-            InnerEle ele = map.get(key);
-            last=last==ele?last.upper:last;
-            
-            // 去掉原来的元素
-            ele.upper.next=ele.next;
-            move2First(ele);
-            return map.get(key).value;
-        }
-        return -1;
-    }
-    
-    public void put(int key, int value) {
-        if(map.containsKey(key)){
-            InnerEle ele = map.get(key);
-            last=last==ele?last.upper:last;
-            ele.upper.next=ele.next;
-            move2First(ele);
-            if(ele.value!=value){
-                ele.value=value;
-            }
-        }else{
-            InnerEle ele = new InnerEle(key,value);
-            move2First(ele);
-            if(++currSize>capacity){
-                last=last.upper;
-                last.next=null;
-            }
-        }
-    }
-    
-    public void move2First(InnerEle ele){
-        InnerEle temp = start.next;
-        start.next=ele;
-        
-        ele.upper=start;
-        ele.next=temp;
-        
-        temp.upper=ele;
-    }
-}
